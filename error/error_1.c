64e76be9cbc37f900665930984f29190e3f3a69c
ComplicatedPhenomenon <czfzdxx@outlook.com>
2017-04-09 10:05:41 +0800

    confess

---
+++ error/error_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main()
+{
+	int dividend = 1;
+	int divisor = 0;
+	int quotient;
+
+	if ( divisor == 0){
+		fprintf(stderr, "Division by zero! Exiting...\n");
+		exit(-1);
+	}
+}	
