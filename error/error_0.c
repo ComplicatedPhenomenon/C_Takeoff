d87684ff8e966b22dd6670a1f8f4101971cb60fa
ComplicatedPhenomenon <czfzdxx@outlook.com>
2017-04-20 18:08:46 +0800

    k

--- error/error_0.c
+++ error/error_0.c
@@ -7,6 +7,9 @@ extern int errno;
 int main()
 {
 	FILE * pf;
+/*When accessing files through C, the first necessity is to have a way to
+ * access the files. For C File I/O you need to use a FILE pointer, which 
+ * will let the program keep track of the file being accessed. */
 	int errnum;
 	pf = fopen("exist.txt","rb");
 	if (pf == NULL)
