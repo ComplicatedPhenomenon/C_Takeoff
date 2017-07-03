d02dcd3c053bfa0475c6e5e50c7cfca7ce9d726c
ComplicatedPhenomenon <czfzdxx@outlook.com>
2017-07-03 21:55:25 +0800

    origin force

---
+++ algorithm/monte_1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+   printf("%d",RAND_MAX);
+   return 0;
+}
+
+/*************************************************
+ * You don't need to define `int RAND_MAX`       */
