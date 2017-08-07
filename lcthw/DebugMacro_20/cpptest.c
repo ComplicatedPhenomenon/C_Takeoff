1232a4e6ef2120f16f21cb298a643e1be417c823
ComplicatedPhenomenon <czfzdxx@outlook.com>
2017-08-07 17:21:55 +0800

    debug macro

---
+++ lcthw/DebugMacro_20/cpptest.c
@@ -0,0 +1,6 @@
+#define SET_FLAGS(w,f) ((w)->flags | (f))
+
+int main(void)
+	SET_FLAGS(0, 4771);
+	SET_FLAGS(wm, wangmiao);
+	return 0;
--- lcthw/DebugMacro_20/dbg.h
+++ lcthw/DebugMacro_20/dbg.h
@@ -4,27 +4,34 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
-
 #ifdef NDEBUG
-#define debug(M,...)
+#define debug(arg,...)
+/*... means the number of remain arguments is unknow*/
 #else   //The alternative for #if
-#define debug(M,...)fprintf(stderr, "DEBUG %s :%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
-#endif  //Ends preprocessor conditional
+#define debug(arg,...)fprintf(stderr, "DEBUG %s :%d: " arg "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+/*##__VA_ARGS__ tell the preprocess to replace what's in ##__VA_ARGS__ with what's in the position ...
+ * log_err("Age: %d, name:%s", age, name); expand into
+ * fprintf(stderr,"[ERROR](%s :%d: errno:%s Age: %d, name: %s \n", __FILE__, __LINE__, clean_errno(), age, name */
+#endif  
 
-#define clean_errno()(errno == 0? "None": strerror(errno))
-#define log_err(M,...)fprintf(stderr, "[ERROR] (%s :%d: errno: %s)" M "\n", __FILE__, __LINE__, clean_errno(),##__VA_ARGS__)
+#define clean_errno()(errno == 0? "None": strerror(errno)) 
+/* use strerror() get an human-readable string to the error number 
+ * a ? b : c if a is logically true (does not evaluate to zero) then 
+ * evaluate expression b, otherwise evaluate expression c */
 
-#define log_warn(M,...)fprintf(stderr, "[WARN] (%s :%d: errno: %s)" M "\n", __FILE__, __LINE__, clean_errno(),##__VA_ARGS__)
+#define log_err(arg,...)fprintf(stderr, "[ERROR] (%s :%d: errno: %s)" arg "\n", __FILE__, __LINE__, clean_errno(),##__VA_ARGS__)
 
-#define log_info(M,...)fprintf(stderr, "[INFO] (%s :%d)" M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_warn(arg,...)fprintf(stderr, "[WARN] (%s :%d: errno: %s)" arg "\n", __FILE__, __LINE__, clean_errno(),##__VA_ARGS__)
 
-#define check(A, M,...) if(!(A)){log_err(M, ##__VA_ARGS__); errno = 0; goto error;}
+#define log_info(arg,...)fprintf(stderr, "[INFO] (%s :%d)" arg "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 
-#define sentinel(M,...) {log_err(M, ##__VA_ARGS__);errno = 0;  goto error;}
+#define check(A, arg,...) if(!(A)){log_err(arg, ##__VA_ARGS__); errno = 0; goto error;}
+
+#define sentinel(arg,...) {log_err(arg, ##__VA_ARGS__);errno = 0;  goto error;}
 
 #define check_mem(A) check((A), "Out of memory.")
 
-#define check_debug(A,M, ...) if(!(A)){ debug(M, ##__VA_ARGS__); errno = 0; goto error; }
+#define check_debug(A,arg, ...) if(!(A)){ debug(arg, ##__VA_ARGS__); errno = 0; goto error; }
 
 #endif
 
